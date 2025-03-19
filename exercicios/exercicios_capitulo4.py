2bce06fe2f58afb6d98565999913b18692dcbe0a
biavianadev <107958093+biavianadev@users.noreply.github.com>
2025-03-19 13:42:26 -0300

    Corrigindo exercicios_capitulo4.py

--- exercicios/exercicios_capitulo4.py
+++ exercicios/exercicios_capitulo4.py
@@ -7,6 +7,7 @@ if x % 2 == 0:
 else:
     print(f'O número {x} é ímpar')
 
+
 # 2. Escreva um programa que leia dois inteiros e mostre na tela apenas o menor dos dois. Se ambos forem iguais, mostre qualquer um deles.
 y = int(input('Digite o primeiro número: '))
 z = int(input('Digite o segundo número: '))
@@ -17,12 +18,14 @@ elif z < y:
 else:
     print(f'Os dois números são iguais e valem {y}')
 
+
 # 3. Escreva um programa que leia a idade de uma pessoa e mostre se está de acordo ou não com a classificação indicativa de 16 anos.
 w = int(input('Digite a sua idade: '))
 if w >= 16:
     print('Você está de acordo com a classificação de 16 anos')
 else:
     print('Você não está de acordo com a classificação de 16 anos')
+
 
 # 4. Escreva um programa que leia um número inteiro e mostre na tela se ele é divisível por 10 ou não.
 n = int(input('Digite um número: '))
